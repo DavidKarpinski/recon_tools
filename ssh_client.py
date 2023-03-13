49b14320c906130badc487765eba0187fcacac55
David Eduardo Karpinski <105662189+DavidKarpinski@users.noreply.github.com>
2023-03-13 14:38:57 -0300

    Create ssh_client.py

---
+++ ssh_client.py
@@ -0,0 +1,18 @@
+import paramiko
+
+
+host = '127.0.0.1' # Change this
+user = 'kali' # Change this
+passwd = 'kali' # Change this
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, username=user, password=passwd)
+
+while True:
+    stdin, stdout, stderr = client.exec_command(input('ssh> '))
+    for line in stdout.readlines():
+        print(line.strip())
+    errors = stderr.readlines()
+    if errors:
+        print(errors)
